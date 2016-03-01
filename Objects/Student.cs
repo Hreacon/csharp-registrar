0dd803d0ac03672ce0f61d074e4e286f8d1b91b9
Nicholas Jensen and Matt Newell <mnewell10@gmail.com>
2016-03-01 09:13:17 -0800

    Add first test and Student cs

--- Objects/Student.cs
+++ Objects/Student.cs
@@ -2,14 +2,45 @@ using System.Collections.Generic;
 using System;
 using System.Data;
 using System.Data.SqlClient;
-using JensenNS.Objects;
 
 namespace RegistrarNS.Objects
 {
-  public class Student : DBHandler
+  public class Student
   {
-    public Student()
+    private int _id;
+    private string _name;
+    private DateTime _date;
+
+    public Student(string name, DateTime date, int id =0)
     {
+      _id = id;
+      _name = name;
+      _date = date;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public void SetId(int id)
+    {
+      _id = id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string name)
+    {
+      _name = name;
+    }
+    public DateTime GetDate()
+    {
+      return _date;
+    }
+    public void SetDate(DateTime date)
+    {
+      _date = date;
     }
   } // end class
 } // end namespace
--- Tests/RegistrarTest.cs
+++ Tests/RegistrarTest.cs
@@ -9,13 +9,19 @@ namespace RegistrarNS
 {
   public class StudentTest : IDisposable
   {
+    [Fact]
+    public void Student_StudentHoldsOwnName()
+    {
+      Student testStudent = new Student("matt", new DateTime(1/4/2016));
+      Assert.Equal("matt", testStudent.GetName());
+    }
      public StudentTest()
      {
        DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=harvard_test;Integrated Security=SSPI;";
      }
      public void Dispose()
      {
-       Registrar.DeleteAll();
+      //  Student.DeleteAll();
      }
   }
 }
