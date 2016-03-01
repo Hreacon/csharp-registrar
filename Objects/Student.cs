1eae56b5f7abcb15ef44334f2bb0e427f034d72b
Nicholas Jensen and Matt Newell <mnewell10@gmail.com>
2016-03-01 11:24:18 -0800

    Add Save GetAll Equals DeleteAll

--- Objects/Student.cs
+++ Objects/Student.cs
@@ -17,7 +17,20 @@ namespace RegistrarNS.Objects
       _name = name;
       _date = date;
     }
-
+    public override bool Equals(System.Object otherStudent)
+    {
+      if (!(otherStudent is Student))
+      {
+        return false;
+      }
+      else
+      {
+        Student testStudent = (Student) otherStudent;
+        bool idEquality =this.GetId() == testStudent.GetId();
+        bool nameEquality = this.GetName() == testStudent.GetName();
+        return (idEquality && nameEquality);
+      }
+    }
     public int GetId()
     {
       return _id;
@@ -41,6 +54,130 @@ namespace RegistrarNS.Objects
     public void SetDate(DateTime date)
     {
       _date = date;
+    }
+
+    public static List<Student> GetAll()
+    {
+      List<Student> allStudents = new List<Student>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM students", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int studentId = rdr.GetInt32(0);
+        string studentName = rdr.GetString(1);
+        DateTime StudentDateTime = rdr.GetDateTime(2);
+        Student testStudent = new Student(studentName, StudentDateTime, studentId);
+        allStudents.Add(testStudent);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allStudents;
+    }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO students (name, date) OUTPUT INSERTED.id VALUES (@StudentName, @StudentDate)", conn);
+
+      SqlParameter nameParam = new SqlParameter();
+      nameParam.ParameterName = "@StudentName";
+      nameParam.Value = this.GetName();
+
+      SqlParameter dateParam = new SqlParameter();
+      dateParam.ParameterName = "@StudentDate";
+      dateParam.Value = this.GetDate();
+
+      cmd.Parameters.Add(nameParam);
+      cmd.Parameters.Add(dateParam);
+
+
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    // Student student1 = Student.Find(1);
+
+    public static Student Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM students WHERE id = @StudentId", conn);
+
+      SqlParameter idParam = new SqlParameter();
+      idParam.ParameterName = "@StudentId";
+      idParam.Value = id;
+
+      cmd.Parameters.Add(idParam);
+
+      rdr = cmd.ExecuteReader();
+
+      int foundStudentId = 0;
+      string foundStudentName = null;
+      DateTime foundStudentdate = new DateTime(2000,1,1);
+
+      while(rdr.Read())
+      {
+        foundStudentId = rdr.GetInt32(0);
+        foundStudentName = rdr.GetString(1);
+        foundStudentdate = rdr.GetDateTime(2);
+      }
+      Student foundStudent = new Student(foundStudentName, foundStudentdate, foundStudentId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundStudent;
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("Delete FROM students", conn);
+
+      rdr = cmd.ExecuteReader();
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
     }
   } // end class
 } // end namespace
--- Tests/RegistrarTest.cs
+++ Tests/RegistrarTest.cs
@@ -12,16 +12,47 @@ namespace RegistrarNS
     [Fact]
     public void Student_StudentHoldsOwnName()
     {
-      Student testStudent = new Student("matt", new DateTime(1/4/2016));
+      Student testStudent = new Student("matt", new DateTime(2016,1,4));
       Assert.Equal("matt", testStudent.GetName());
     }
+
+    [Fact]
+    public void Equals_Override_Student()
+    {
+      Student testStudent = new Student("matt", new DateTime(2016,1,4));
+      Student testStudent2 = new Student("matt", new DateTime(2016,1,4));
+      Assert.Equal(testStudent2, testStudent);
+    }
+
+    [Fact]
+    public void Student_GetAll()
+    {
+      Assert.Equal(0, Student.GetAll().Count);
+    }
+
+    [Fact]
+    public void Student_Saves()
+    {
+      Student testStudent = new Student("matt", new DateTime(2016,1,4));
+      testStudent.Save();
+      Assert.Equal(1, Student.GetAll().Count);
+    }
+
+    [Fact]
+    public void Student_FindsById()
+    {
+      Student test = new Student("matt", new DateTime(1991,8,8));
+      test.Save();
+      Assert.Equal(test, Student.Find(test.GetId()));
+    }
+
      public StudentTest()
      {
        DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=harvard_test;Integrated Security=SSPI;";
      }
      public void Dispose()
      {
-      //  Student.DeleteAll();
+       Student.DeleteAll();
      }
   }
 }
